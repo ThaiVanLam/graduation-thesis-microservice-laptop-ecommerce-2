500208ebdae820b27e09e0ab2f93c38292750795
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-26 10:16:19 +0700

    439. Sending Cart to Backend From Frontend

--- order-service/src/main/java/com/ecommerce/order_service/service/CartServiceImpl.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/CartServiceImpl.java
@@ -18,6 +18,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
