e0b29902611232cfcc3573cd29626e02fc1263b2
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-29 08:58:39 +0700

    462. Payment Intent with Customer Profile - Part 2

--- order-service/src/main/java/com/ecommerce/order_service/service/StripeServiceImpl.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/StripeServiceImpl.java
@@ -3,7 +3,11 @@ package com.ecommerce.order_service.service;
 import com.ecommerce.order_service.payload.StripePaymentDto;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.model.CustomerSearchResult;
 import com.stripe.model.PaymentIntent;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.CustomerSearchParams;
 import com.stripe.param.PaymentIntentCreateParams;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
@@ -24,9 +28,37 @@ public class StripeServiceImpl implements StripeService {
 
     @Override
     public PaymentIntent paymentIntent(StripePaymentDto stripePaymentDto) throws StripeException {
+        Customer customer;
+
+        CustomerSearchParams searchParams =
+                CustomerSearchParams.builder()
+                        .setQuery("email:'" + stripePaymentDto.getEmail() + "'")
+                        .build();
+        CustomerSearchResult customers = Customer.search(searchParams);
+
+        if (customers.getData().isEmpty()) {
+            CustomerCreateParams customerParams =
+                    CustomerCreateParams.builder()
+                            .setName(stripePaymentDto.getName())
+                            .setEmail(stripePaymentDto.getEmail())
+                            .setAddress(CustomerCreateParams.Address.builder()
+                                    .setLine1(stripePaymentDto.getAddress().getStreet())
+                                    .setCity(stripePaymentDto.getAddress().getCity())
+                                    .setState(stripePaymentDto.getAddress().getState())
+                                    .setPostalCode(stripePaymentDto.getAddress().getPincode())
+                                    .setCountry(stripePaymentDto.getAddress().getCountry())
+                                    .build())
+                            .build();
+            customer = Customer.create(customerParams);
+        } else {
+            customer = customers.getData().get(0);
+        }
+
         PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
                 .setAmount(stripePaymentDto.getAmount())
                 .setCurrency(stripePaymentDto.getCurrency())
+                .setCustomer(customer.getId())
+                .setDescription(stripePaymentDto.getDescription())
                 .setAutomaticPaymentMethods(
                         PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
                                 .setEnabled(true)
